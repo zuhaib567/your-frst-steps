96b1103efb4e35a0d3fa749b2c3cc77380ad3d77
Muhammad Zohaib <zohaibmughal343@gmail.com>
2026-05-09 13:59:39 +0500

    chore: ui enhancement

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import tailwindcss from '@tailwindcss/vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [
@@ -11,10 +12,11 @@ export default defineConfig({
     tsConfigPaths(),
     tanstackStart(),
     netlify(),
+    react({ jsxRuntime: "automatic" }), // ← add this
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
